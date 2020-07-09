a5fccae280cf1773d7d255a35d52f1f9b53ad352
Sazzad Hossain Khan <itsazzad@gmail.com>
2020-07-09 21:22:12 +0600

    short description system config

---
+++ Block/Adminhtml/System/Config/Form/Field/Editor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Harriswebworks\Sizing\Block\Adminhtml\System\Config\Form\Field;
+
+use Magento\Framework\Registry;
+use Magento\Backend\Block\Template\Context;
+use Magento\Cms\Model\Wysiwyg\Config as WysiwygConfig;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Editor extends \Magento\Config\Block\System\Config\Form\Field
+{
+    public function __construct(
+        Context $context,
+        WysiwygConfig $wysiwygConfig,
+        array $data = []
+    )
+    {
+        $this->_wysiwygConfig = $wysiwygConfig;
+        parent::__construct($context, $data);
+    }
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $element->setWysiwyg(true);
+        $element->setConfig($this->_wysiwygConfig->getConfig(array('add_variables' => true, 'add_widgets' => false, 'add_images' => true)));
+        return parent::_getElementHtml($element);
+    }
+}
